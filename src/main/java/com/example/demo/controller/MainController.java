da52aa1c0fe370a4611fafed60612598cade242a
nolzhou <mingzitaisu@163.com>
2021-01-15 11:08:17 +0800

    version 3.0

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -23,20 +23,24 @@ public class MainController {
 
     @PostMapping(path = "/register")
     public MyResponseEntity register(@RequestBody UserBody body){
+        System.out.println("成功接收注册请求");
         if(mainService.countByName(body.getName())!=0){
+            System.out.println("注册失败");
             return new MyResponseEntity(-1,"register failed",null);
         }
         UserEntity userEntity=new UserEntity();
         userEntity.setName(body.getName());
         userEntity.setPassword(body.getPassword());
         mainService.save(userEntity);
+        System.out.println("注册成功");
         return new MyResponseEntity(1,"register successfully",null);
     }
 
     @PostMapping(path = "/login")
     public MyResponseEntity login(@RequestBody UserBody body){
-        System.out.println("成功接收");
-        if(mainService.countByName(body.getName())==0){
+        System.out.println("成功接收登录请求");
+        if(mainService.countByName(body.getName())!=1){
+            System.out.println("登录失败");
             return new MyResponseEntity(-1,"login failed",null);
         }
         UserEntity currentUser=mainService.findByName(body.getName());
@@ -44,18 +48,21 @@ public class MainController {
             System.out.println("登录成功");
             return new MyResponseEntity(1,"login successfully",null);
         }else {
+            System.out.println("登录失败");
             return new MyResponseEntity(-1,"login failed",null);
         }
     }
 
     @PostMapping(path = "/bind")
     public MyResponseEntity bind(@RequestBody BindUserBody body){
-        System.out.println("成功接收手机号");
+        System.out.println("成功接收绑定手机号请求");
         mainService.updateByName(body.getTelephone(),body.getName());
         String telephone=mainService.findTelephoneByName(body.getName());
         if(telephone==null||telephone.isEmpty()){
+            System.out.println("绑定失败");
             return new MyResponseEntity(-1,"bind failed",null);
         }else {
+            System.out.println("绑定成功");
             return new MyResponseEntity(1,"bind successfully",null);
         }
     }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -1,16 +1,14 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.UserEntity;
-import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Repository
+@Transactional
 public interface UserRepository extends JpaRepository<UserEntity,Integer> {
 
     @Query(value = "select * from user where name = ?1",nativeQuery = true)
@@ -32,4 +30,5 @@ public interface UserRepository extends JpaRepository<UserEntity,Integer> {
     @Query(value = "select telephone from user where name = ?1", nativeQuery = true)
     String findTelephoneByName(String name);
 
+    UserEntity save(UserEntity userEntity);
 }
